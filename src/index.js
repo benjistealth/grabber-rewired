b1aa91f62384c8af2fc5a8ddfa53eb67555eb8c2
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-03-16 14:22:49 +0000

    added Shanes code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import 'bulma/css/bulma.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
