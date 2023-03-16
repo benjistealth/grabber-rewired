bf2bd6ace70f861b0b99eabc32a2e6712d306c85
Toby King <tobyking13@gmail.com>
2023-03-16 23:16:45 +0000

    sets recipe card width. sets footer at the bottom of the viewport. adds some text to footer.

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,6 +1,16 @@
 function Footer() {
+    const style = {
+        textAlign: 'center',
+        width: '100vw',
+        backgroundColor: 'black !important',
+        position: 'absolute',
+        bottom: '0px'
+    }
     return (
-        <div>👌</div>
+        <div style={style}>
+        <h4>© 2023<strong>{`   <Grabber Rewired />   👌`}</strong> </h4>
+        
+        </div>
     );
   }
   
