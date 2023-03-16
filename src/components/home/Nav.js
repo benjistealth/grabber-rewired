dc1e35e2317fe51f7f944c73c164b96d35c14c83
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-03-16 13:49:27 +0000

    undid logo change

--- src/components/home/Nav.js
+++ src/components/home/Nav.js
@@ -1,19 +1,17 @@
-import grabberLogo from '../../assets/Images/grabberLogo.jpg'
-
 function Nav() {
-    return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-        <img src={grabberLogo} alt="Grabber rewired logo" width="150" height="10" />
-    </a>
+  return (
+  <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+      <a className="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
+  </a>
 
-    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" href="./" >
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+  <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" href="./" >
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+  </a>
+</div>
 </nav>
 )
 }
