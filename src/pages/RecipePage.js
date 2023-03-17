a1f15fc44afd7577db128387b869064dacbed4e1
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-03-17 16:58:47 +0000

    added csspage for search section and API key for extra calls

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,15 +1,17 @@
 import React  from "react";
 import { useNavigate } from 'react-router-dom';
 import printJS from "print-js";
-import "../css/RecipePage.css"
+import "../css/RecipePage.css";
+import Ingredients from "../utils/Ingredients.js";
 
 
 
 function RecipePage() {
   const navigate = useNavigate();
-
   const individualRecipe = JSON.parse(localStorage.getItem("individual-result"));
-  console.log(individualRecipe.image)
+  // console.log(individualRecipe.image)
+  Ingredients(individualRecipe.id);
+  console.log(Ingredients);
 	const GoBack = () => {
 		navigate(-1);
   }
@@ -20,7 +22,7 @@ function RecipePage() {
       type: "html"
     });
   }
-
+  console.log(printRecipe)
   return (
     <div>
       <button className="btn btn-back" onClick={GoBack}>Go Back</button>
