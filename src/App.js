ae4faa0e4bc5e96da42763552627acbe565cd298
splwebdev <shane_lynch@hotmail.com>
2023-03-18 16:46:21 +0000

    added guest pages removing print, nutritional and serving settings

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import Nav from "./components/home/Nav";
 import Jumbotron from "./components/home/Jumbotron";
 import RecipeSearchContainer from "./pages/RecipeSearchContainer";
+import RecipeSearchGuest from "./pages/RecipeSearchGuest";
 import Footer from "./components/home/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipePage from "./pages/RecipePage";
+import RecipePageGuest from "./pages/RecipePageGuest";
 import TestingPage from "./pages/TestingPage";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
@@ -21,13 +23,15 @@ function App() {
         <Router>
           <div>
             <Routes>
-              <Route path="/RecipeSearchContainer" element={<RecipeSearchContainer />} />
               <Route path="/" element={<Home />} />
               <Route path="/SignUp" element={<SignUp />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/Testing" element={<TestingPage />} />
-              <Route path="/Login" element={<Login />} /> */
+              <Route path="/Login" element={<Login />} />
               <Route path="/RecipePage" element={<RecipePage />} />
+              <Route path="/RecipeSearchContainer" element={<RecipeSearchContainer />} />
+              <Route path="/RecipePageGuest" element={<RecipePageGuest />} />
+              <Route path="/RecipeSearchGuest" element={<RecipeSearchGuest />} />
             </Routes>
           </div>
         </Router>
---
+++ src/pages/RecipePageGuest.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+// import Nutrition from "../components/home/Nutrition";
+import "../css/RecipePage.css";
+
+function RecipePageGuest() {
+  const navigate = useNavigate();
+  const individualRecipe = JSON.parse(localStorage.getItem("individual-result"));
+
+  const recipeSteps = individualRecipe.analyzedInstructions[0].steps;
+  const ingredients = individualRecipe.extendedIngredients;
+  const GoBack = () => {
+    navigate("/RecipeSearchGuest");
+  }
+
+  return (
+    <div className="maincontainer">
+      {/* <button className="btn btn-back" onClick={handleClick}>Nutrition</button> */}
+      <button className="btn btn-back" onClick={GoBack}>
+        Go Back
+      </button>
+      <br />
+      <br />
+      <div className="recipe" id="printRecipe">
+        <h1 className="foodTitle">{individualRecipe.title}</h1>
+        <br />
+        <div className="imageContainer">
+          <img
+            src={individualRecipe.image}
+            alt={individualRecipe.title}
+            className="selectedImage"
+          />
+        </div>
+        <div className="container infocontainer">
+          <div className="col-md-6 ms-4 mt-5 mb-5 instructionscontainer">
+            <h1 className="titleHeader">Instructions</h1>
+            {recipeSteps.map((recipeStep, index) => (
+              <p className="instructions mt-2" key={index}>{`${index + 1}.)  ${
+                recipeStep.step
+              }`}</p>
+            ))}
+          </div>
+          <div className="col-md-3 ms-5 mt-5 mb-5 ingredientscontainer">
+            <h1 className="titleHeader">Ingredients</h1>
+            {ingredients.map((ingredient, index) => (
+              <p className="ingredients mt-2" key={index}>{`${index + 1}.) ${ingredient.amount}${ingredient.unit} ${ingredient.name}`}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RecipePageGuest;
