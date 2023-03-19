f34b564fb1b570205089186f684e18a7dbcad2cf
splwebdev <shane_lynch@hotmail.com>
2023-03-19 11:32:52 +0000

    added media queries

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -67,22 +67,22 @@ function RecipePage() {
     <div className="container maincontainer">
       {/* <button className="btn btn-back" onClick={handleClick}>Nutrition</button> */}
       <div className="btn-box">
-      <div className="btn-box-bk">
-      <button className="btn btn-back" onClick={GoBack}>
-        Go Back
-      </button>
-      <br />
-      </div>
-      <div className="btn-box-util">
-      <button className="btn btn-print" onClick={printRecipe}>
-        Print Recipe
-      </button>
-      <br />
-      <button className="btn btn-email" onClick={EmailRecipe}>
-        Email yourself THIS recipe
-      </button>
-      <br />
-      </div>
+        <div className="btn-box-bk">
+          <button className="btn btn-back" onClick={GoBack}>
+            Go Back
+          </button>
+          <br />
+        </div>
+        <div className="btn-box-util">
+          <button className="btn btn-print" onClick={printRecipe}>
+            Print Recipe
+          </button>
+          <br />
+          <button className="btn btn-email" onClick={EmailRecipe}>
+            Email yourself THIS recipe
+          </button>
+          <br />
+        </div>
       </div>
       <div className="recipe" id="printRecipe">
         <h1 className="foodTitle">{individualRecipe.title}</h1>
@@ -95,27 +95,40 @@ function RecipePage() {
           />
         </div>
         <div className="container infocontainer">
-          <div className="col-md-2 ms-4 mt-5 mb-5 instructionscontainer">
+          <div className="col-md-2 ms-3 mt-5 mb-5 instructionscontainer">
             <h1 className="titleHeader">Nutrition</h1>
             <p>{`Carbs: ${caloricBreakdown.percentCarbs}%`}</p>
             <p>{`Fat: ${caloricBreakdown.percentFat}%`}</p>
             <p>{`Protein: ${caloricBreakdown.percentProtein}%`}</p>
-
           </div>
-          <div className="col-md-6 ms-4 mt-5 mb-5 instructionscontainer">
+          <div className="col-lg-6 col-md-4 col-sm-4 ms-3 mt-5 mb-5 instructionscontainer">
             <h1 className="titleHeader">Instructions</h1>
             {recipeSteps.map((recipeStep, index) => (
-              <p className="instructions mt-2" key={index}>{`${index + 1}.)  ${recipeStep.step
-                }`}</p>
+              <p className="instructions mt-2" key={index}>{`${index + 1}.)  ${
+                recipeStep.step
+              }`}</p>
             ))}
           </div>
-          <div className="col-md-3 ms-5 mt-5 mb-5 ingredientscontainer">
+          <div className="col-md-3 col-sm-3 ms-4 mt-5 mb-5 ingredientscontainer">
             <h1 className="titleHeader">Ingredients</h1>
             <label htmlFor="customRange2">{`Serving size: ${servingSize}`}</label>
-            <input type="range" className="custom-range" min="1" max="20" id="customRange2" value={servingSize} onChange={handleSlider}></input>
+            <input
+              type="range"
+              className="custom-range"
+              min="1"
+              max="20"
+              id="customRange2"
+              value={servingSize}
+              onChange={handleSlider}
+            ></input>
             {ingredients.map((ingredient, index) => (
-              <p className="ingredients mt-2" key={index}>{`${index + 1}.) ${(ingredient.amount = ingredient.amount * (servingSize / individualRecipe.servings)).toFixed(1)
-                } ${ingredient.unit} ${ingredient.name}`}</p>
+              <p className="ingredients mt-2" key={index}>{`${
+                index + 1
+              }.) ${(ingredient.amount =
+                ingredient.amount *
+                (servingSize / individualRecipe.servings)).toFixed(1)} ${
+                ingredient.unit
+              } ${ingredient.name}`}</p>
             ))}
           </div>
         </div>
