0d999acf5bbc20798eba4e3acd48203e0847c40d
Toby King <tobyking13@gmail.com>
2023-03-21 18:40:57 +0000

    adds toggle switch for light/dark theme

--- src/components/HomeComponents/Jumbotron/Jumbotron.js
+++ src/components/HomeComponents/Jumbotron/Jumbotron.js
@@ -1,6 +1,8 @@
 import JumbotronTitle from "./JumbotronTitle";
 import JumbotronImage from "./JumbotronImage";
-import Toggle from "./Toggle";
+import Toggle from "../Switch/Toggle";
+import SunIcon from "../Jumbotron/assets/icons/sunIcon";
+import MoonIcon from "../Jumbotron/assets/icons/moonIcon";
 import { useNavigate } from "react-router-dom";
 import "./Jumbotron.css"
 
@@ -26,8 +28,14 @@ function Jumbotron() {
     <section className="hero jumbo is-medium is-link">
       <div className="hero-body">
         <JumbotronImage />
-        <JumbotronTitle />
-        <Toggle />
+        <div>
+          <JumbotronTitle />
+          <div className="theme-switch">
+            <SunIcon />
+            <Toggle />
+            <MoonIcon />
+          </div>
+        </div>
         <button className="btn btn-logout" onClick={Logout}>
           Logout
         </button>
---
+++ src/components/HomeComponents/Switch/Toggle.js
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from "react";
+import { ThemeContext } from "../../../Theme/Theme";
+
+import "./Toggle.css";
+
+function Toggle() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [isToggled, setIsToggled] = useState(theme);
+
+  const onToggle = () => {
+    console.log("onToggle called");
+    setIsToggled(!isToggled);
+    toggleTheme();
+  };
+
+  return (
+    <label className="toggle-switch">
+      <input type="checkbox" defaultChecked={true} onChange={onToggle} />
+      <span className="switch" />
+    </label>
+  );
+}
+
+export default Toggle;
