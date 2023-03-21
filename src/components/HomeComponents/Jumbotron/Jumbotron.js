5a09396733c5eae1aba22696e5b94ba1566f199d
Ben <stealthton@gmail.com>
2023-03-21 18:18:14 +0000

    logout with rough button

--- src/components/HomeComponents/Jumbotron/Jumbotron.js
+++ src/components/HomeComponents/Jumbotron/Jumbotron.js
@@ -1,16 +1,36 @@
 import JumbotronTitle from "./JumbotronTitle";
 import JumbotronImage from "./JumbotronImage";
 import Toggle from "./Toggle";
+import { useNavigate } from "react-router-dom";
 import "./Jumbotron.css"
 
 function Jumbotron() {
-  
+
+  const navigate = useNavigate();
+
+  const Logout = () => {
+
+    if (localStorage.getItem("loggedin") !== null) {
+      let loggedIn = localStorage.getItem("loggedin");
+      console.log(loggedIn);
+      if (loggedIn) {
+        localStorage.setItem("loggedin", false);
+        navigate('/');
+        console.log("user logged out");
+      }
+    }
+
+  }
+
   return (
     <section className="hero jumbo is-medium is-link">
       <div className="hero-body">
         <JumbotronImage />
         <JumbotronTitle />
         <Toggle />
+        <button className="btn btn-logout" onClick={Logout}>
+          Logout
+        </button>
       </div>
     </section>
   );
